bae7c9e4ed27817a7e944e8855b9169969eb1969
Robert <robert@alvianus.net>
2021-09-16 00:40:41 +0800

    update..

--- phase1-collector/module-vrni-collector/main.tf
+++ phase1-collector/module-vrni-collector/main.tf
@@ -22,41 +22,27 @@ data "vsphere_network" "network" {
 	datacenter_id = data.vsphere_datacenter.datacenter.id
 }
 
-data "vsphere_ovf_vm_template" "ovf" {
-	name             = var.vm_name
-	resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
-	datastore_id     = data.vsphere_datastore.datastore.id
-	host_system_id   = data.vsphere_host.host.id
-	remote_ovf_url   = var.remote_ovf_url
-
-	ovf_network_map = {
-		"VM Network" = data.vsphere_network.network.id
-	}
-}
-
 resource "vsphere_virtual_machine" "vm" {
 	datacenter_id	= data.vsphere_datacenter.datacenter.id
-	name		= data.vsphere_ovf_vm_template.ovf.name
+	name		= var.vm_name
 	num_cpus	= data.vsphere_ovf_vm_template.ovf.num_cpus
 	memory		= data.vsphere_ovf_vm_template.ovf.memory
 	guest_id	= data.vsphere_ovf_vm_template.ovf.guest_id
 	wait_for_guest_net_timeout = 0
   wait_for_guest_ip_timeout = 0
-	resource_pool_id = data.vsphere_ovf_vm_template.ovf.resource_pool_id
-	datastore_id     = data.vsphere_ovf_vm_template.ovf.datastore_id
-	host_system_id   = data.vsphere_ovf_vm_template.ovf.host_system_id
+	resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
+	datastore_id     = data.vsphere_datastore.datastore.id
+	host_system_id   = data.vsphere_host.host.id
 
-	dynamic "network_interface" {
-		for_each = data.vsphere_ovf_vm_template.ovf.ovf_network_map
-		content {
-			network_id = network_interface.value
+	"network_interface" {
+		"VM Network" = data.vsphere_network.network.id
 		}
 	}
 	ovf_deploy {
 		disk_provisioning	= "thin"
 		ovf_network_map		= data.vsphere_ovf_vm_template.ovf.ovf_network_map
 		deployment_option = var.deployment_option
-		remote_ovf_url		= data.vsphere_ovf_vm_template.ovf.remote_ovf_url
+		remote_ovf_url		= var.remote_ovf_url
 	}
 	vapp {
 		properties = {
