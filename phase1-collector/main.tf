35f0e3f8f7100c126c4f3e1f63c59f58fa2af022
Robert <robert@alvianus.net>
2021-09-16 00:32:21 +0800

    update..

--- phase1-collector/main.tf
+++ phase1-collector/main.tf
@@ -16,7 +16,7 @@ module "vrni-platform" {
 	### vsphere variables
 	datacenter	= "lab01"
 	cluster		= "mgmt"
-	host		= "esx11.lab01.one"
+	host		= "esx15.lab01.one"
 	datastore	= "vsanDatastore "
 	network		= "pg-mgmt"
 
